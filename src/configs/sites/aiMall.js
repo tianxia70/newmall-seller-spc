857a95372d5f8537ac861c40e0b635044b2a8a34
liuliufei <liuliufei@gmail.com>
2025-06-05 23:57:30 +0800

    aiMall 盘口配置

---
+++ src/configs/sites/aiMall.js
@@ -0,0 +1,10 @@
+import logo from '@/assets/logos/aiMall.png';
+import favicon from '@/assets/favicons/aiMall.ico';
+
+export default {
+  title: 'Ai Mall',
+  logo,
+  favicon,
+  siteAreaCodeDefalut: 44,
+  themeColor: '#1552F0'
+}
