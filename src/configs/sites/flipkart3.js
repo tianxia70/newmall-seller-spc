789bd891afee1223e6ca76141b5f0ace94a270e6
liuliufei <liuliufei@gmail.com>
2025-07-26 18:02:13 +0800

    flipkart3 和 shein 新盘配置

---
+++ src/configs/sites/flipkart3.js
@@ -0,0 +1,8 @@
+import logo from '@/assets/logos/flipkart3.png';
+import favicon from '@/assets/favicons/flipkart3.ico';
+
+export default {
+  title: `Flipkart`,
+  logo,
+  favicon
+}
---
+++ src/configs/sites/shein.js
@@ -0,0 +1,8 @@
+import logo from '@/assets/logos/shein.png';
+import favicon from '@/assets/favicons/shein.ico';
+
+export default {
+  title: `SHEIN WHOLESALE`,
+  logo,
+  favicon
+}
