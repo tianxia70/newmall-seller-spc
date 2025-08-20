a13109c9c52e19252632970ea999ad8a67038ca4
liuliufei <liuliufei@gmail.com>
2025-08-21 00:03:58 +0800

    joom 新盘配置

---
+++ src/configs/sites/joom.js
@@ -0,0 +1,10 @@
+import logo from '@/assets/logos/joom.png';
+import favicon from '@/assets/favicons/joom.ico';
+
+export default {
+  title: 'Joom',
+  logo,
+  favicon,
+  siteAreaCodeDefalut: 44,
+  themeColor: '#F64343'
+}
